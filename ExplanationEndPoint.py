0e3be58c2f2eb9e967bb4ba075dae616edf084cf
Yingchao Ji <yingchao.ji@thoughtworks.com>
2023-03-24 10:55:52 +0800

    refactor: delete unnecessary codes

--- ExplanationEndPoint.py
+++ ExplanationEndPoint.py
@@ -7,33 +7,12 @@ from src.getData import getData
 from flask import Flask, render_template
 import pandas as pd
 
-app = app = Flask(__name__)
+app = Flask(__name__)
 autoencoder = load_model('/Users/yingchao.ji/Desktop/tw/thesisProjects/explainable_ai_display/src/models/97.hdf5')
 fraud, non_fraud, X_Train, Y_Train, x_test, df_original, X_test = getData()
 lime_explainer, shap_explainer, train_columns = getExplainers(X_Train, Y_Train, autoencoder)
 
 shap_plots = {}
-chart_labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-chart_data = [1.5, 2.3, 4.3, 2.2, 5.1, 6.5, 2.3, 2.3, 2.2, 1.1]
-chart_data2 = [6.5, 2.3, 2.3, 2.2, 1.1, 1.5, 2.3, 4.3, 2.2, 5.1]
-
-table_columns = [
-    {'title': 'Rule Name', 'dataIndex': 'name'},
-    {'title': 'Description', 'dataIndex': 'desc'},
-    {'title': '# of Calls', 'dataIndex': 'callNo'},
-    {'title': 'Status', 'dataIndex': 'status'},
-    {'title': 'Updated At', 'dataIndex': 'updatedAt'}
-]
-table_data = [
-    {"callNo": 76,
-     "desc": "Description of Operation",
-     "id": 0,
-     "name": "Alpha",
-     "status": 3,
-     "updatedAt": "2019-12-13"
-     }
-]
-
 
 @app.route('/charts/<number>/<data>')
 def form_page(number, data):
